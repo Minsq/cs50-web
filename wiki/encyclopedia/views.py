6a56d468503830ed36abb32bc766c38472dc3bb2
min min <minsq.ng@gmail.com>
2021-08-04 01:06:20 +0800

    Create wiki features
    left with editing function to complete

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+import markdown2
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def title(request, title):
+	entry = util.get_entry(title)
+	
+	if entry==None:
+		return render(request, "encyclopedia/error.html",{
+			"title": title,
+			})
+
+	return render(request, "encyclopedia/title.html",{
+		"title": title,
+		"entry": markdown2.markdown(entry)
+		})
+
+
+def search(request):
+	q = request.GET['q']
+
+	entries = util.list_entries()
+	possible_entries = []
+
+	for entry in entries:
+		if q.lower() == entry.lower():
+			return HttpResponseRedirect(reverse('title',args=[entry])) 
+			#redirect('',)
+		elif q.lower() in entry.lower():
+			possible_entries.append({
+				"query" : q,
+				"entry" : entry 
+				})
+	return render(request, "encyclopedia/search.html", 
+		{"possible_entries": possible_entries, "query": q}) 
+
+def create(request):
+	if request.method == "POST":
+		#add to folder
+		title = request.POST['page_title']
+		text_markdown = request.POST['page_markdown']
+		if util.get_entry(title) != None:
+			# Throw an error
+			pass
+		util.save_entry(title, text_markdown)
+		return HttpResponseRedirect(reverse('title',args=[title]))
+
+
+	return render(request, "encyclopedia/create.html")
+
+def edit(request, title):
+	return render(request, "encyclopedia/edit.html")
+
---
+++ wiki/entries/Emphasis.md
@@ -0,0 +1,12 @@
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
